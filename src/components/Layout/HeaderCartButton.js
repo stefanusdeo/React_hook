7bf39851839c4518cce17f6cd77d8ff964c1dab7
stefanusdeo <stefanusdeo07@gmail.com>
2022-02-27 22:35:40 +0700

    finish

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import cartContext from '../../store/cart-context';
 // icon
 import CartIcon from '../Cart/CartIcon';
@@ -6,13 +6,31 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 export default function HeaderCartButton(props) {
+  const [btnHighlight, setBtnHighlight] = useState(false);
   const ctx = useContext(cartContext);
+
+  const btnClasses = `${classes.button} ${btnHighlight ? classes.bump : ''}`;
 
   const allCartItem = ctx.item.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
+
+  useEffect(() => {
+    if (ctx.item.length === 0) {
+      return;
+    }
+    setBtnHighlight(true);
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.item]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
